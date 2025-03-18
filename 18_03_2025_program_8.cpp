08c2205dd56aea4081910a4e8a798cf4272298f3
mdsameerCrescent <240071601096@crescent.education>
2025-03-18 12:28:55 +0530

    Update 18_03_2025_program_8.cpp

--- 18_03_2025_program_8.cpp
+++ 18_03_2025_program_8.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+class Base {
+public:
+    virtual void show() { // Virtual function
+        cout << "Base class show() method" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void show() override { // Overriding the base class method
+        cout << "Derived class show() method" << endl;
+    }
+};
+
+int main() {
+    Base* obj;  // Pointer to base class
+    Derived d;
+
+    obj = &d;  // Base class pointer pointing to Derived class object
+    obj->show(); // Calls the overridden method in Derived class
+
+    return 0;
+}
